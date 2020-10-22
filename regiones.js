cd7cddb68258fa993fa5f120d44429bd441d8e32
Dereemii <leshermira@gmail.com>
2020-10-22 00:22:57 -0300

    adding jquery

---
+++ regiones.js
@@ -0,0 +1,21 @@
+
+    $("#ship-state option[value='I Región']").append(' - Tarapaca');
+    $("#ship-state option[value='II Región']").append(' - Antofagasta');
+    $("#ship-state option[value='III Región']").append(' - Atacama');
+    $("#ship-state option[value='IV Región']").append(' - Coquimbo');
+    $("#ship-state option[value='V Región']").append(' - Valparaiso');
+    $("#ship-state option[value='VI Región']").append(" - O'Higgins");
+    $("#ship-state option[value='IX Región']").append(' - La Araucanía');
+    $("#ship-state option[value='VII Región']").append(' - Maule');
+    $("#ship-state option[value='VIII Región']").append(' - BíoBío');
+    $("#ship-state option[value='X Región']").append(' - Los Lagos');
+    $("#ship-state option[value='XI Región']").append(' - Aysén');
+    $("#ship-state option[value='XII Región']").append(' - Magallanes');
+    $("#ship-state option[value='XIV Región']").append(' - Los Ríos');
+    $("#ship-state option[value='XV Región']").append(' - Arica y Parinacota');
+    $("#ship-state option[value='XVI Región']").append(' - Ñuble');
+
+
+
+
+
